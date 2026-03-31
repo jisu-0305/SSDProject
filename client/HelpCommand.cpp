7def1100f11de954bda7835063a606ed044b1a3e
DESKTOP-8KU9VIQ\906_09 <mh3603@naver.com>
2026-03-31 10:47:59 +0900

    "Help command" 오류 수정

--- client/HelpCommand.cpp
+++ client/HelpCommand.cpp
@@ -35,11 +35,11 @@ int HelpCommand::validate()
 
 int HelpCommand::run()
 {
-    std::cout << "팀 이름: 쌀먹 삼대장";
+    std::cout << "팀 이름: 쌀먹 삼대장 ";
 	std::cout << "팀  원: 이민한, 김지수, 오승훈 ";
-	std::cout << "각 명령어 사용 법";
+	std::cout << "각 명령어 사용 법 ";
 
-	std::cout << "[Write]: write 숫자 value를 넣으세요 ex): write 3 0x11223344" << std::endl;
+	std::cout << "[Write]: write 숫자 value를 넣으세요 ex): write 3 0x11223344 " << std::endl;
 	std::cout << "\n";
 	std::cout<<"[Read]: write 숫자를 넣으세요 ex): read 3" << std::endl;
 	std::cout << "\n";
